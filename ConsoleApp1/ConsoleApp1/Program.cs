70d22b4b783c3a465b8e82bd2083143e29dd6d23
Kids08 <Kids08@G529-12>
2022-10-08 13:02:00 +0300

    олщрт

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "250";
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа через запятую");
+            var a = Console.ReadLine();
+            var arr = a.Split(',');
+            foreach (var i in arr)
+            {
+                int result = 0;
+                if (!int.TryParse(i, out result))
+                {
+                    Console.WriteLine("Ошибка");
+                }
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
